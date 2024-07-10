2b13e02d45ef67fcdc802127f62541d5e46c0273
Shubham <shivu00soni@gmail.com>
2024-07-10 11:08:43 +0530

    Set up of project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+          <div className="bg-black">
+            <h1>REACT</h1>
+          </div>
+    
+  )
+}
+
+export default App
